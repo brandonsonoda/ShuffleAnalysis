f371ed81e1af3f4eb8a7379dfdb3c3240715e640
Brandon Sonoda <sonodabe@gmail.com>
2023-09-04 16:56:13 -0400

    Test analyzer with computer style shuffle

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    ShuffleAnalyzer.analyze(deck -> {}, 10, 100);
+    ShuffleAnalyzer.analyze(Main::computerShuffle, 8, 9999999);
+  }
+
+  private static void computerShuffle(Deck d) {
+    for (int i = 0; i < d.cards.length; i++) {
+      int swapLocation = i + (int)(Math.random() * (d.cards.length - i));
+      int buffer = d.cards[i];
+      d.cards[i] = d.cards[swapLocation];
+      d.cards[swapLocation] = buffer;
+    }
   }
 }
